4c08d5ba93d796c58f623ef940e8602512479de5
APf0x <77631767+APf0x@users.noreply.github.com>
2021-05-21 09:35:18 +0200

    Update snake_sense_hat.py

--- snake_sense_hat.py
+++ snake_sense_hat.py
@@ -2,8 +2,8 @@ from sense_hat import SenseHat
 import time
 import random
 
-sensehat = SenseHat()
-sensehat.low_light = True
+sense = SenseHat()
+sense.low_light = True
 #qui metto dei colori che andro a usare 
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
@@ -98,32 +98,32 @@ while True:
 
     # qui comincia quel count down e richiamo la variabile images che ora fa un bell efetto
     for img in images:
-        sensehat.set_pixels(img())
+        sense.set_pixels(img())
         time.sleep(UNA_PAUSA)
 
     # una posizione default per il serpente forse dovrei smettere di sctivere tuttu questi commenti
-    snakePosX = [3]
-    snakePosY = [6]
+    posx = [3]
+    posy = [6]
 
     # questa parte serve per mettere in posizioni random il cibo o pallini rossi 
     while True:
         foodPosX = random.randint(0, 7)
         foodPosY = random.randint(0, 7)
-        if foodPosX != snakePosX[0] or foodPosY != snakePosY[0]:
+        if foodPosX != posx[0] or foodPosY != posy[0]:
             break
 
     # questo come la posizione default e la direzione in cui si dirige default anche se non mi piace questa 
     # ps alen cambia la posizione e trovane una migliore quando avrai tempo 
     # si mi sono totalmente dimenticato di cambiare sta posizione va be lo faccio domani
-    movementX = 0
-    movementY = -1
+    x = 0
+    y = -1
 
 
     # qui comincia il vero e proprio gioco non ho nemmeno cominciato e sto gia bestammiando magnifico
     # comunque questo e il loop del gioco
     while not gameOverFlag:
         # questo è un piccolo ceck se mangia ste palline o no
-        if foodPosX == snakePosX[0] and foodPosY == snakePosY[0]:
+        if foodPosX == posx[0] and foodPosY == posy[0]:
             growSnakeFlag = True
             generateRandomFoodFlag = True
             snakeMovementDelay += snakeMovementDelayDecrease
@@ -131,8 +131,8 @@ while True:
         # questo serve a capire se ti mangi da solo perche scrivo sti commenti tanto me lo ricordo va be
         #forse passo al inglese bho non so nemmeno se funziona
         #update sta roba non so perche ma funziona e non ho intenzione di toccarla se funziona funziona
-        for i in range(1, len(snakePosX)):
-            if snakePosX[i] == snakePosX[0] and snakePosY[i] == snakePosY[0]:
+        for i in range(1, len(posx)):
+            if posx[i] == posx[0] and posy[i] == posy[0]:
                 gameOverFlag = True
 
         # proff non mi amazzi dovevo mettere questo break come faccievo mettevo un or nell while dell inizio del loop
@@ -143,48 +143,48 @@ while True:
         # ok dopo 3 ore di documentazione credo di aver capito come funziona sta roba ora dovrebbe teoricamente
         # funzionare credo ...
         # update: tutto funzionante ^^ oggi sono proprio felicie 
-        events = sensehat.stick.get_events()
+        events = sense.stick.get_events()
         for event in events:
-            if event.direction == "left" and movementX != 1:
-                movementX = -1
-                movementY = 0
+            if event.direction == "left" and x != 1:
+                x = -1
+                y = 0
                 
-            elif event.direction == "right" and movementX != -1:
-                movementX = 1
-                movementY = 0
+            elif event.direction == "right" and x != -1:
+                x = 1
+                y = 0
                 
-            elif event.direction == "up" and movementY != 1:
-                movementY = -1
-                movementX = 0
+            elif event.direction == "up" and y != 1:
+                y = -1
+                x = 0
                 
-            elif event.direction == "down" and movementY != -1:
-                movementY = 1
-                movementX = 0
+            elif event.direction == "down" and y != -1:
+                y = 1
+                x = 0
                 
 
         # quanto il serpente mangia qualcosa diventa piu grande giusto  ecco questa è la funzione di questa parte del codice
         if growSnakeFlag:
             growSnakeFlag = False
-            snakePosX.append(0)
-            snakePosY.append(0)
+            posx.append(0)
+            posy.append(0)
 
         # questo è il continuo movimento del serpente mi ci è voluto un po per capire sto len
-        for i in range((len(snakePosX) - 1), 0, -1):
-            snakePosX[i] = snakePosX[i - 1]
-            snakePosY[i] = snakePosY[i - 1]
+        for i in range((len(posx) - 1), 0, -1):
+            posx[i] = posx[i - 1]
+            posy[i] = posy[i - 1]
 
-        snakePosX[0] += movementX
-        snakePosY[0] += movementY
+        posx[0] += x
+        posy[0] += y
 
         # qui mette i bordi della mappa quindi se vai a un limite ti fa ritornare indietro
-        if snakePosX[0] > UN_PO_PIU_PRIMA_DEL_BORDO:
-            snakePosX[0] -= VAI_INDIETRO_QUANDO_SEI_SUL_BORDO
-        elif snakePosX[0] < UN_PO_PIU_PRIMA_DEL_BORDO_A_DESTRA:
-            snakePosX[0] += VAI_INDIETRO_QUANDO_SEI_SUL_BORDO
-        if snakePosY[0] > UN_PO_PIU_PRIMA_DEL_BORDO:
-            snakePosY[0] -= VAI_INDIETRO_QUANDO_SEI_SUL_BORDO
-        elif snakePosY[0] < UN_PO_PIU_PRIMA_DEL_BORDO_A_DESTRA:
-            snakePosY[0] += VAI_INDIETRO_QUANDO_SEI_SUL_BORDO
+        if posx[0] > UN_PO_PIU_PRIMA_DEL_BORDO:
+            posx[0] -= VAI_INDIETRO_QUANDO_SEI_SUL_BORDO
+        elif posx[0] < UN_PO_PIU_PRIMA_DEL_BORDO_A_DESTRA:
+            posx[0] += VAI_INDIETRO_QUANDO_SEI_SUL_BORDO
+        if posy[0] > UN_PO_PIU_PRIMA_DEL_BORDO:
+            posy[0] -= VAI_INDIETRO_QUANDO_SEI_SUL_BORDO
+        elif posy[0] < UN_PO_PIU_PRIMA_DEL_BORDO_A_DESTRA:
+            posy[0] += VAI_INDIETRO_QUANDO_SEI_SUL_BORDO
 
         # questo serve a far spawnare cibo in posizioni random
         if generateRandomFoodFlag:
@@ -194,16 +194,16 @@ while True:
                 foodPosX = random.randint(0, 7)
                 foodPosY = random.randint(0, 7)
                 retryFlag = False
-                for x, y in zip(snakePosX, snakePosY):
+                for x, y in zip(posx, posy):
                     if x == foodPosX and y == foodPosY:
                         retryFlag = True
                         break
 
-        
-        sensehat.clear()
-        sensehat.set_pixel(foodPosX, foodPosY, RED)
-        for x, y in zip(snakePosX, snakePosY):
-            sensehat.set_pixel(x, y, GREEN)
+        # update matrix:
+        sense.clear()
+        sense.set_pixel(foodPosX, foodPosY, RED)
+        for x, y in zip(posx, posy):
+            sense.set_pixel(x, y, GREEN)
 
-        
+        # snake speed (game loop delay):
         time.sleep(snakeMovementDelay)
